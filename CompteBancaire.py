37b9af29ee9725f9c9f2206ee64c225a3d69cfb6
gaouimounir <mounir.gaoui@outlook.com>
2024-02-01 18:02:32 +0100

    feat: :art: creation methode de class

--- CompteBancaire.py
+++ CompteBancaire.py
@@ -2,3 +2,7 @@ class CompteBancaire:
     def __init__(self, numeroCompte, solde):
         self.numeroCompte = numeroCompte
         self.solde = solde
+
+    @classmethod
+    def augmenterSolde(cls, compte, montant):
+        return list(map(lambda compte: cls(compte.numeroCompte, compte.solde + montant), compte))
