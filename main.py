bd455532b7d62a1d68356987a8d17de78a51701e
gaouimounir <mounir.gaoui@outlook.com>
2024-02-03 14:54:22 +0100

    feat: :zap: utilisation methode static appliquer interet

--- main.py
+++ main.py
@@ -4,12 +4,14 @@ compte1 = CompteBancaire(123, 500)
 compte2 = CompteBancaire(456, 1000)
 compte3 = CompteBancaire(789, 1500)
 
+listeComptes = [compte1, compte2, compte3]
+
+
 print("Solde des comptes : ")
 print(compte1.solde)
 print(compte2.solde)
 print(compte3.solde)
 
-listeComptes = [compte1, compte2, compte3]
 
 listeComptes = CompteBancaire.augmenterSolde(listeComptes, 100)
 
@@ -18,3 +20,8 @@ for compte in listeComptes:
     print(compte.solde)
 
 
+CompteBancaire.appliquerInteret(listeComptes, 5)
+
+print("Nouveau solde des comptes : ")
+for compte in listeComptes:
+    print(compte.solde)
